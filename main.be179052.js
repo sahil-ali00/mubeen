e4f71b8c526d0de2ff4c7390312fb0a86fb231c8
sahil-ali00 <snnizamani12@gmail.com>
2025-06-02 12:00:39 -0700

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -9814,7 +9814,7 @@
                                 className: "ant-form ant-form-horizontal css-m4timi",
                                 onSubmit: e => {
                                     e.preventDefault(),
-                                    fetch("https://submit-form.com/Q1WLgqT5E", {
+                                    fetch("https://submit-form.com/PztXVg8Gk", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13358,7 +13358,7 @@
                             he.jsxs)("form", {
                                 onSubmit: n => {
                                     n.preventDefault(),
-                                    u && o ? (fetch("https://submit-form.com/Q1WLgqT5E", {
+                                    u && o ? (fetch("https://submit-form.com/PztXVg8Gk", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
@@ -13471,7 +13471,7 @@
                 he.jsx)("button", {
                     onClick: n => {
                         n.preventDefault(),
-                        fetch("https://submit-form.com/Q1WLgqT5E", {
+                        fetch("https://submit-form.com/PztXVg8Gk", {
                             method: "POST",
                             headers: {
                                 "Content-Type": "application/json"
